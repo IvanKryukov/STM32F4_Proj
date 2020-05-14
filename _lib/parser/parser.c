991350222e9953d14b46a41163eaecc41f18987a
Ivan Kryukov <Iwan.Kryukow@yandex.ru>
2020-05-15 00:07:45 +0300

    cmd parameter definer

--- _lib/parser/parser.c
+++ _lib/parser/parser.c
@@ -6,55 +6,55 @@
 
 parser_struct parser_line;
 
+/***** Variables *****/
+
 unsigned char cmd_idx_names[ALL_COMMANDS][16] = 
 {
-	"unknown",
-	"help",
-	"log"
+	"unknown\0",
+	"help\0",
+	"list\0"
 };
 
 unsigned char cmd_parser_prm_name[ALL_PARAMETERS][10] = 
 { 
-	"!"       // single argument
+	" ", 	// unknown parameter
+	"h"	// help - to get help of command
 };
 
-unsigned char start_line[256];
+unsigned char start_line[256], sub_line[32];
+
+/*********************/
+
+static unsigned char* select_subline(unsigned char *src, unsigned char *dst)
+{
+	memset(sub_line, 0, 32);
+	dst = &sub_line[0];
+	
+	if (*src == ' ')
+		src++;
+	
+	while ((*src != ' ') && (*src != 0))
+		*dst++ = *src++;
+	
+	return src;
+}
 
 void prs_copy_to_start_line(unsigned char *extern_line)
 {
+	int i = 0;
+	
+	while (*extern_line)
+		start_line[i++] = *extern_line++;
+}
+
+static cmd_idx_type define_cmd_parser_index(unsigned char *cmd_line)
+{
 	int i;
-	for (i = 0; i < 255; i++)
-		start_line[i] = *extern_line++;
-}
-
-void print_cmd_parser_struct(parser_struct *parser_print)
-{
-	//uint8_t i;
-	prs_print("\n\r""command description:");
-	prs_print("\n\rname: \t");
-	prs_print(parser_print->cmd_idx_name);
-	prs_print("\n\rindex: \t");
-	prs_print_int(parser_print->cmd_idx);/*
-	for (i = 0; i < 8; i++) 
-	{
-		if (parser_print->cmd_prm[i] == -1) break;
-		printf("\nparam(%d): \t%d", i, parser_print->cmd_prm[i]);
-	}
-	printf("\nint_arg: \t%d", parser_print->int_argument[0]);
-	printf("\nint_arg: \t%d", parser_print->int_argument[1]);
-	printf("\nchr_arg: \t%s\n", parser_print->chr_argument);*/
-}
-
-cmd_idx_type define_cmd_parser_index(unsigned char *cmd_line)
-{
-	int i, j, k = 0;
-	unsigned char *p = cmd_line, *pp, c1, c2;
-	unsigned int numbers[ALL_COMMANDS];
-
+	
 	for (i = 0; i < ALL_COMMANDS; i++) 
 	{
-		if ((strstr((const char *)start_line, (const char *)cmd_idx_names[i]) != NULL) && 
-				(strcmp((const char *)start_line, (const char *)cmd_idx_names[i]) == 0)) 
+		if ((strstr((const char *)cmd_line, (const char *)cmd_idx_names[i]) != NULL) && 
+				(strcmp((const char *)cmd_line, (const char *)cmd_idx_names[i]) == 0)) 
 		{
 			return ((cmd_idx_type) i);
 		}
@@ -62,39 +62,77 @@ cmd_idx_type define_cmd_parser_index(unsigned char *cmd_line)
 	return cmd_unknown;	
 }
 
+static int define_cmd_parser_parameter(unsigned char *cmd_line, 
+                                       parser_struct *user_parser_struct)
+{
+	int i;
+	unsigned char minus_line[1] = "-";
+    
+	if (strstr((const char *)cmd_line, (const char *)minus_line) != NULL) {
+		for (i = 0; i < ALL_PARAMETERS; i++) {
+			if (strstr((const char *)cmd_line, (const char *)cmd_parser_prm_name[i]) != NULL) {
+				user_parser_struct->cmd_prm = (cmd_prm_type)i;
+			}
+		}
+	}
+	else {
+		return -1; 
+	}
+	
+  return 0;
+}
+
 void parser_routine(char *status)
 {
+	unsigned char *psrc = &start_line[0], *pdst = &sub_line[0];
+	int res = 0;
+	
 	if (*status != 0)
 	{
-		*status = 0;
-		parser_line.cmd_idx = define_cmd_parser_index(start_line);
+		*status = 0;		
 		
-		// command action START
+		/****** catch the command ******/
+		psrc = select_subline(psrc, pdst);
+		parser_line.cmd_idx = define_cmd_parser_index(pdst);
+		
+		/***** catch the parameter *****/
+		psrc = select_subline(psrc, pdst);
+		res = define_cmd_parser_parameter(pdst, &parser_line);
+		
+		/***** catch the argument ******/
 		
 		switch (parser_line.cmd_idx)
 		{
 			case cmd_get_help:
-				prs_print("--- help ---"NL
-									"list of commands"NL
-									"1: help - general command list"NL
-									"2: log  - get log of system"NL);
+				prs_print("##########################################"NL
+									"#                                        #"NL
+									"#     #   # #####  #### ####   ###       #"NL
+									"#    /# #/   /   /     /   / /   /       #"NL
+									"#   / # /   /   /     ####  /   /        #"NL
+									"#  /   /   /   /     / #   /   /         #"NL
+									"# /   / #####  #### /   #  ###           #"NL
+									"#                                        #"NL
+									"#     ####   ###  ####   #### ##### #### #"NL
+									"#    /   / /   / /   / /     /     /   / #"NL
+									"#   ####  ##### ####   ###  ##### ####   #"NL
+									"#  /     /   / / #       / /     / #     #"NL
+									"# /     /   / /   # ####  ##### /   #    #"NL
+									"#                                        #"NL
+									"##########################################"NL
+									NL
+									"Structure of command: "NL
+									"[command] [-p] [argument]"NL
+									" -----------------------"NL
+									"[command] - word of several signs (example: help, log, set, send...)"NL
+									"[-p] - parameter of command, it contains an only sign (example: -p, -m... )"NL
+									"[argument] - word of numeric or symbolic signs to specialize commands"NL
+									);
 				break;
 			
-			case cmd_get_log:
-				prs_print("    #   # #####  #### ####   ### "NL
-									"   /# #/   /   /     /   / /   /"NL
-									"  / # /   /   /     ####  /   /"NL
-									" /   /   /   /     / #   /   /"NL
-									"/   / #####  #### /   #  ### "NL
-									NL
-									"    ####   ###  ####   #### ##### #### "NL
-									"   /   / /   / /   / /     /     /   /"NL
-									"  ####  ##### ####   ###  ##### #### "NL
-									" /     /   / / #       / /     / #  "NL
-									"/     /   / /   # ####  ##### /   #"NL
-									NL
-									"tiny parser for microcontroller"NL
-									);
+			case cmd_get_list:
+				prs_print("list of commands"NL
+									"1: help - general help"NL
+									"2: list - get list of system"NL);
 				break;
 			
 			default:
@@ -102,7 +140,17 @@ void parser_routine(char *status)
 				break;
 		}
 		
-		// command action END
+	/*
+		switch (parser_line.cmd_prm)
+		{
+			case: 
+				
+				break;
+			
+			default:				
+				prs_print("unknown parameter!"NL);
+				break;
+		}*/
 		
 		prs_print(NL">> ");
 	}
--- _lib/parser/parser.h
+++ _lib/parser/parser.h
@@ -11,28 +11,27 @@ typedef enum
 {
 	cmd_unknown		= 0,
 	cmd_get_help	= 1,
-	cmd_get_log		= 2
+	cmd_get_list	= 2
+	//add new command
 } cmd_idx_type;
 
 typedef enum
 {
 	prm_unknown			= 0,
 	prm_first_param	= 1
+	// add new param
 } cmd_prm_type;
 
 typedef struct _parser_struct 
 {
     unsigned char		cmd_idx_name[256];
     cmd_idx_type		cmd_idx;
-    cmd_prm_type		cmd_prm[8];
+    cmd_prm_type		cmd_prm;
     unsigned int		int_argument[2];
     unsigned char		chr_argument[512];
 } parser_struct;
 
 void prs_copy_to_start_line(unsigned char* extern_line);
-void print_cmd_parser_struct(parser_struct *parser_print);
-cmd_idx_type define_cmd_parser_index(unsigned char *cmd_line);
-
 void parser_routine(char *status);
 
 #endif
